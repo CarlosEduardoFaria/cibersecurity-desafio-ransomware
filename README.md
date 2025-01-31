55433acf7b898bc1e5e039a88b25c184b2f650fc
CarlosEduardoFaria <eduardostark@icloud.com>
2025-01-31 02:02:52 -0300

    Evolutiva para aceitar o nome do arquivo como argumento e retornar o nome original ao descriptografar.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Código para encriptar / decriptar arquivos
+
+## Comandos:
+
+### 1 - Encriptar
+>>> python encrypter.py mensagem-importante.txt
+
+### 2 - Decriptar
+>>> python decrypter.py mensagem-importante.txt.ransomware
+
+#### Obs: é necessário estar dentro do mesmo diretório para a execução funcionar
+
+#### Dependências: 
+>>> pip install pyaes
--- decrypter.py
+++ decrypter.py
@@ -1,9 +1,12 @@
+import sys
 import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+file_name = sys.argv[1]
+#file_name = "mensagem-importante.txt.ransomware"
+
+file = open(file_name, "rb") ##rb = read
 file_data = file.read()
 file.close()
 
@@ -16,7 +19,7 @@ decrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+new_file = file_name.replace(".ransomware", "")
+new_file = open(f'{new_file}', "wb") #wb = write
 new_file.write(decrypt_data)
 new_file.close()
